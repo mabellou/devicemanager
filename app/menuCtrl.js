80b8aa7d407004d6e36a1d92e03c4c3b53aefc46
marcvermeir <marcvermeir@pandora.be>
2016-11-27 15:13:16 +0100

    20161126

--- app/menuCtrl.js
+++ app/menuCtrl.js
@@ -1,8 +1,12 @@
 
-app.controller("menuCtrl", function($scope, $location) {
+app.controller("menuCtrl", function($scope, $location, Creds) {
 
   $scope.menuClass = function(page) {
     var current = $location.path();
     return page === current ? "blog-nav-item active" : "blog-nav-item";
+  };
+
+  $scope.userName = function() {
+    return Creds.getCredentials().username;
   };
 });
